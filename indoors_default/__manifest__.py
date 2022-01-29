787879591c68d63416c6a8d180a9a2c23621532e
leobozzi <lbozzi@gmail.com>
2022-01-29 12:16:05 -0300

    Initial commit

--- indoors_default/__manifest__.py
+++ indoors_default/__manifest__.py
@@ -294,11 +294,11 @@
         'https://github.com/ingadhoc/account-financial-tools ingadhoc-account-financial-tools',  # noqa
         'https://github.com/ingadhoc/account-payment ingadhoc-account-payment',
         # 'https://github.com/ingadhoc/aeroo_reports ingadhoc-aeroo_reports',
-        # 'https://github.com/ingadhoc/argentina-reporting ingadhoc-argentina-reporting',
+        'https://github.com/ingadhoc/argentina-reporting ingadhoc-argentina-reporting',
         'https://github.com/ingadhoc/argentina-sale ingadhoc-argentina-sale',
-        # 'https://github.com/ingadhoc/hr ingadhoc-hr',
-        # 'https://github.com/ingadhoc/miscellaneous ingadhoc-miscellaneous',
-        # 'https://github.com/ingadhoc/multi-company ingadhoc-multi-company',
+        'https://github.com/ingadhoc/hr ingadhoc-hr',
+        'https://github.com/ingadhoc/miscellaneous ingadhoc-miscellaneous',
+        'https://github.com/ingadhoc/multi-company ingadhoc-multi-company',
         # 'https://github.com/ingadhoc/multi-store ingadhoc-multi-store',
 
         # Fix porque falla la instalacion de l10n_ar_ux
@@ -306,14 +306,14 @@
 
         # 'https://github.com/ingadhoc/odoo-argentina-ce ingadhoc-odoo-argentina-ce',
         'https://github.com/ingadhoc/odoo-argentina-ee ingadhoc-odoo-argentina-ee',
-        # 'https://github.com/ingadhoc/partner ingadhoc-partner',
-        # 'https://github.com/ingadhoc/product ingadhoc-product',
+        'https://github.com/ingadhoc/partner ingadhoc-partner',
+        'https://github.com/ingadhoc/product ingadhoc-product',
         # 'https://github.com/ingadhoc/project ingadhoc-project',
-        # 'https://github.com/ingadhoc/purchase ingadhoc-purchase',
-        # 'https://github.com/ingadhoc/reporting-engine ingadhoc-reporting-engine',
-        # 'https://github.com/ingadhoc/sale ingadhoc-sale',
-        # 'https://github.com/ingadhoc/stock ingadhoc-stock',
-        # 'https://github.com/ingadhoc/website ingadhoc-website',
+        'https://github.com/ingadhoc/purchase ingadhoc-purchase',
+        'https://github.com/ingadhoc/reporting-engine ingadhoc-reporting-engine',
+        'https://github.com/ingadhoc/sale ingadhoc-sale',
+        'https://github.com/ingadhoc/stock ingadhoc-stock',
+        'https://github.com/ingadhoc/website ingadhoc-website',
     ],
     'docker-images': [
        'odoo lbozzi/odoo-docker-lb:15.0',
