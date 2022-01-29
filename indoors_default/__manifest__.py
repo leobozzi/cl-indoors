f051e9005c6216a5051320c5df52cd00b69fdba7
leobozzi <lbozzi@gmail.com>
2022-01-29 09:04:38 -0300

    Initial commit

--- indoors_default/__manifest__.py
+++ indoors_default/__manifest__.py
@@ -18,7 +18,7 @@
 #
 # -----------------------------------------------------------------------------------
 {
-    'name': 'indoors',
+    'name': 'Indoors',
     'version': '15.0.1.0.0',
     'license': 'Other OSI approved licence',
     'category': 'Tools',
@@ -26,24 +26,24 @@
     'author': 'Leonardo Bozzi',
     'depends': [
         # Applicaciones del Cliente
-        'sale',
-        'sale_management',
+        # 'sale',
+        # 'sale_management',
         # 'sale_order_type',
         # 'sale_order_type_ux',
-        'purchase',
-        'stock',
-        'hr',
-        'hr_expense',
-        'note',
-        'board',
-        'calendar',
-        'website',
-        'crm',
+        # 'purchase',
+        # 'stock',
+        # 'hr',
+        # 'hr_expense',
+        # 'note',
+        # 'board',
+        # 'calendar',
+        # 'website',
+        # 'crm',
         # 'sale_crm_ux',
         # 'crm_stage_teams',
 
         # Contabilidad
-        'account',
+        # 'account',
         # 'account_ux',
         # 'account_check',
         # 'account_financial_report',
@@ -65,13 +65,14 @@
         # 'purchase_requisition',
         # 'purchase_requisition_stock',
 
+
         # Localización
         # All
-        'l10n_ar',
-        'l10n_ar_bank',
-        'l10n_ar_account_withholding',
-        'l10n_ar_sale',
-        'l10n_ar_stock',
+        # 'l10n_ar',
+        # 'l10n_ar_bank',
+        # 'l10n_ar_account_withholding',
+        # 'l10n_ar_sale',
+        # 'l10n_ar_stock',
         # 'padron_afip',
         # 'report_qweb_pdf_watermark',
 
@@ -84,13 +85,6 @@
         # 'l10n_ar_reports',
         # 'l10n_ar_aeroo_base',
 
-        # EE
-        'l10n_ar_edi',
-        'l10n_ar_reports',
-        'l10n_ar_account_tax_settlement',
-        'l10n_ar_edi_ux',
-        'l10n_ar_currency_update',
-
         # HR
         # 'hr_timesheet',
         # 'hr_timesheet_sheet',
@@ -102,8 +96,8 @@
         # 'web_search_with_and',
         # 'web_advanced_search',
         # 'auto_backup',
-        'mass_editing',
         # 'intero_reload_form',
+        # 'mass_editing',
         # 'web_company_color',
         # 'backend_theme_v13',
         # 'odoo_whatsapp_integration',
@@ -212,8 +206,8 @@
     ],
 
     'git-repos': [
-        'https://github.com/leobozzi/cl-hormimax.git',
-        'https://github.com/leobozzi/odoo-addons-utils bit-odoo-addons-utils',
+        'https://github.com/leobozzi/cl-indoors.git',
+        #'https://github.com/leobozzi/odoo-addons-utils bit-odoo-addons-utils',
 
         # Odoomates
         # ==========================================================================================
@@ -221,36 +215,37 @@
 
         # Gabriela Rivero
         # ==========================================================================================
-        'https://github.com/regaby/odoo-custom regaby-odoo-custom',
+        #'https://github.com/regaby/odoo-custom regaby-odoo-custom',
 
         # OCA
         # ==========================================================================================
         # 'https://github.com/OCA/account-closing oca-account-closing',
-        'https://github.com/OCA/account-financial-reporting oca-account-financial-reporting',  # noqa
-        'https://github.com/OCA/account-financial-tools oca-account-financial-tools',
-        'https://github.com/OCA/account-payment oca-account-payment',
+        # 'https://github.com/OCA/account-financial-reporting oca-account-financial-reporting',  # noqa
+        # 'https://github.com/OCA/account-financial-tools oca-account-financial-tools',
+        # 'https://github.com/OCA/account-payment oca-account-payment',
         # 'https://github.com/OCA/apps-store oca-apps-store',
         # 'https://github.com/OCA/bank-payment oca-bank-payment',
-        'https://github.com/OCA/bank-statement-import oca-bank-statement-import',
-        'https://github.com/OCA/brand oca-brand',
+        # 'https://github.com/OCA/bank-statement-import oca-bank-statement-import',
+        # 'https://github.com/OCA/brand oca-brand',
         # 'https://github.com/OCA/business-requirement oca-business-requirement',
         # 'https://github.com/OCA/commission oca-commission',
         # 'https://github.com/OCA/contract oca-contract',
         # 'https://github.com/OCA/credit-control oca-credit-control',
-        'https://github.com/OCA/crm oca-crm',
+        # 'https://github.com/OCA/crm oca-crm',
         # 'https://github.com/OCA/currency oca-currency',
         # 'https://github.com/OCA/ddmrp oca-ddmrp',
         # 'https://github.com/OCA/delivery-carrier oca-delivery-carrier',
         # 'https://github.com/OCA/e-commerce oca-e-commerce',
         # 'https://github.com/OCA/event oca-event',
-        'https://github.com/OCA/field-service oca-field-service',
-        'https://github.com/OCA/geospatial oca-geospatial',
+        # 'https://github.com/OCA/field-service oca-field-service',
+        # 'https://github.com/OCA/geospatial oca-geospatial',
         # 'https://github.com/OCA/helpdesk oca-helpdesk',
-        'https://github.com/OCA/hr oca-hr',
-        'https://github.com/OCA/hr-timesheet oca-hr-timesheet',
+        # 'https://github.com/OCA/hr oca-hr',
+        # 'https://github.com/OCA/hr-timesheet oca-hr-timesheet',
         # 'https://github.com/OCA/knowledge oca-knowledge',
+        # 'https://github.com/OCA/stock-logistics-warehouse oca-stock-logistics-warehouse'
         # 'https://github.com/OCA/management-system oca-management-system',
-        'https://github.com/OCA/manufacture oca-manufacture',
+        # 'https://github.com/OCA/manufacture oca-manufacture',
         # 'https://github.com/OCA/manufacture-reporting oca-manufacture-reporting',
         # 'https://github.com/OCA/margin-analysis oca-margin-analysis',
         # 'https://github.com/OCA/multi-company oca-multi-company',
@@ -262,25 +257,25 @@
         # 'https://github.com/OCA/product-pack oca-product-pack',
         # 'https://github.com/OCA/project oca-project',
         # 'https://github.com/OCA/project-reporting oca-project-reporting',
-        'https://github.com/OCA/purchase-workflow oca-purchase-workflow',
+        # 'https://github.com/OCA/purchase-workflow oca-purchase-workflow',
         # 'https://github.com/OCA/queue oca-queue',
         # 'https://github.com/OCA/report-print-send oca-report-print-send',
-        'https://github.com/OCA/reporting-engine oca-reporting-engine',
+        # 'https://github.com/OCA/reporting-engine oca-reporting-engine',
         # 'https://github.com/OCA/sale-reporting oca-sale-reporting',
-        'https://github.com/OCA/sale-workflow oca-sale-workflow',
+        # 'https://github.com/OCA/sale-workflow oca-sale-workflow',
         # 'https://github.com/OCA/server-auth oca-server-auth',
         # 'https://github.com/OCA/server-backend oca-server-backend',
-        'https://github.com/OCA/server-tools oca-server-tools',
-        'https://github.com/OCA/server-ux oca-server-ux',
+        # 'https://github.com/OCA/server-tools oca-server-tools',
+        # 'https://github.com/OCA/server-ux oca-server-ux',
         # 'https://github.com/OCA/social oca-social',
         # 'https://github.com/OCA/stock-logistics-barcode oca-stock-logistics-barcode',
         # 'https://github.com/OCA/stock-logistics-reporting oca-stock-logistics-reporting', # noqa
         # 'https://github.com/OCA/stock-logistics-transport oca-stock-logistics-transport', # noqa
         # 'https://github.com/OCA/stock-logistics-warehouse oca-stock-logistics-warehouse', # noqa
-        'https://github.com/OCA/stock-logistics-workflow oca-stock-logistics-workflow',
+        # 'https://github.com/OCA/stock-logistics-workflow oca-stock-logistics-workflow',
         # 'https://github.com/OCA/timesheet oca-timesheet',
         # 'https://github.com/OCA/vertical-association oca-vertical-association',
-        'https://github.com/OCA/web oca-web',
+        # 'https://github.com/OCA/web oca-web',
         # 'https://github.com/OCA/website oca-website',
         # 'https://github.com/OCA/bank-payment oca-bank-payment',
         # 'https://github.com/OCA/account-analytic',
@@ -288,34 +283,33 @@
         # ADHOC
         # ==========================================================================================
         # 'https://github.com/ingadhoc/account-analytic ingadhoc-account-analytic',
-        'https://github.com/ingadhoc/account-financial-tools ingadhoc-account-financial-tools',  # noqa
-        'https://github.com/ingadhoc/account-payment ingadhoc-account-payment',
-        'https://github.com/ingadhoc/aeroo_reports ingadhoc-aeroo_reports',
-        'https://github.com/ingadhoc/argentina-reporting ingadhoc-argentina-reporting',
-        'https://github.com/ingadhoc/argentina-sale ingadhoc-argentina-sale',
+        # 'https://github.com/ingadhoc/account-financial-tools ingadhoc-account-financial-tools',  # noqa
+        # 'https://github.com/ingadhoc/account-payment ingadhoc-account-payment',
+        # 'https://github.com/ingadhoc/aeroo_reports ingadhoc-aeroo_reports',
+        # 'https://github.com/ingadhoc/argentina-reporting ingadhoc-argentina-reporting',
+        # 'https://github.com/ingadhoc/argentina-sale ingadhoc-argentina-sale',
         # 'https://github.com/ingadhoc/hr ingadhoc-hr',
-        'https://github.com/ingadhoc/miscellaneous ingadhoc-miscellaneous',
+        # 'https://github.com/ingadhoc/miscellaneous ingadhoc-miscellaneous',
         # 'https://github.com/ingadhoc/multi-company ingadhoc-multi-company',
         # 'https://github.com/ingadhoc/multi-store ingadhoc-multi-store',
 
         # Fix porque falla la instalacion de l10n_ar_ux
-        'https://github.com/ingadhoc/odoo-argentina ingadhoc-odoo-argentina',
+        # 'https://github.com/ingadhoc/odoo-argentina ingadhoc-odoo-argentina',
 
-        'https://github.com/ingadhoc/odoo-argentina-ce ingadhoc-odoo-argentina-ce',
+        # 'https://github.com/ingadhoc/odoo-argentina-ce ingadhoc-odoo-argentina-ce',
         # 'https://github.com/ingadhoc/odoo-argentina-ee ingadhoc-odoo-argentina-ee',
         # 'https://github.com/ingadhoc/partner ingadhoc-partner',
-        'https://github.com/ingadhoc/product ingadhoc-product',
+        # 'https://github.com/ingadhoc/product ingadhoc-product',
         # 'https://github.com/ingadhoc/project ingadhoc-project',
         # 'https://github.com/ingadhoc/purchase ingadhoc-purchase',
-        'https://github.com/ingadhoc/reporting-engine ingadhoc-reporting-engine',
-        'https://github.com/ingadhoc/sale ingadhoc-sale',
-        'https://github.com/ingadhoc/stock ingadhoc-stock',
+        # 'https://github.com/ingadhoc/reporting-engine ingadhoc-reporting-engine',
+        # 'https://github.com/ingadhoc/sale ingadhoc-sale',
+        # 'https://github.com/ingadhoc/stock ingadhoc-stock',
         # 'https://github.com/ingadhoc/website ingadhoc-website',
     ],
     'docker-images': [
-       #'odoo jobiols/odoo-jeo:13.0',
-       'odoo lbozzi/odoo-docker-lb:13.0',
-       'postgres postgres:11.1-alpine',
+       'odoo lbozzi/odoo-docker-lb:15.0',
+       'postgres postgres:13.5-alpine',
        'aeroo adhoc/aeroo-docs',
        'nginx nginx',
     ]
